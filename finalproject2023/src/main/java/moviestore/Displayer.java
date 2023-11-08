043ef784cc2ed4134bfce976c3b1596b8d293332
Amy Nguyen <thuc.nguyen1@dawsoncollege.qc.ca>
2023-11-08 10:52:30 -0500

    Made Displayer and BookRentalSystem skeleton classes

---
+++ finalproject2023/src/main/java/moviestore/Displayer.java
@@ -0,0 +1,16 @@
+package moviestore;
+import java.util.*;
+
+public abstract class Displayer{
+
+    /**
+     * 
+     */
+    public List<Movie> showMovies(){
+        throw new UnsupportedOperationException("Not written yet!");
+    }
+
+    public List<Movie> filterMovies(){
+        throw new UnsupportedOperationException("Not written yet!");
+    }
+}
