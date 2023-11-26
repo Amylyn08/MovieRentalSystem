76395da548a18fa536c7085d03995005417a3287
Bianca Rossetti <biancaros05@icloud.com>
2023-11-26 14:41:13 -0500

    implemented discount strategies for 10 & 20 dollars!

--- finalproject2023/src/main/java/moviestore/discounts/TenDollarDiscount.java
+++ finalproject2023/src/main/java/moviestore/discounts/TenDollarDiscount.java
@@ -1,5 +1,14 @@
 package moviestore.discounts;
 
+import moviestore.Customer;
+import moviestore.products.Movie;
+
 public class TenDollarDiscount {
-    
+    public double finalPrice(Customer c, Movie m) {
+        if (c.getPoints() < 15000) {
+            throw new IllegalArgumentException("Customer does not have enough points!");
+        }
+        c.deductPoints(15000);
+        return m.getPrice() - 10;
+    }
 }
---
+++ finalproject2023/src/test/java/moviestore/TestDiscountStrategy.java
@@ -0,0 +1,19 @@
+package moviestore;
+import moviestore.products.*;
+import moviestore.*;
+import moviestore.exceptions.LoaderFailedException;
+import moviestore.discounts.*;
+import static org.junit.Assert.*;
+import java.util.*;
+import java.io.*;
+
+
+public class TestDiscountStrategy {
+    /**
+     * BIANCA
+     */
+
+     /**
+      * AMY
+      */
+}
